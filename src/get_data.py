bbc8e20562617f5c803878e04ebace3c625fa1d6
Nishith Khandwala <nishith@stanford.edu>
2015-03-07 04:46:23 -0800

    Changed data preprocessing. Now have 7 outputs: 1 move_out and 6 move_ins

--- src/get_data.py
+++ src/get_data.py
@@ -16,7 +16,9 @@ games = games[:NUM_GAMES]
 print "Finished loading the PGN file."
 print "Total number of games: %d" % len(games)
 
-all_pairs = []
+move_out = []
+move_in_p1, move_in_p2, move_in_p3 = [], [], []
+move_in_p4, move_in_p5, move_in_p6 = [], [], []
 for index, game in enumerate(games):
 	if index % 100 == 0:
 		print "Processed %d games out of %d" % (index, NUM_GAMES)
@@ -24,31 +26,88 @@ for index, game in enumerate(games):
 	board = chess.Bitboard()
 	moves = game.moves
 
-	previous_board = copy.deepcopy(board)
 	for move_index, move in enumerate(moves):
-		pair = []
-		if move[0].isalpha(): # check if move is SAN
-			board.push_san(move)
+		move_out_pair, move_in_pair = [], []
+		if move[0].isalpha(): # check if move is SAN		
+				
+			from_to_chess_coords = board.parse_san(move)
+			from_to_chess_coords = str(from_to_chess_coords)
+
+			from_chess_coords = from_to_chess_coords[:2]
+			to_chess_coords = from_to_chess_coords[2:4]
+			from_coords = chess_coord_to_coord2d(from_chess_coords)
+			to_coords = chess_coord_to_coord2d(to_chess_coords)
+						
 			if move_index % 2 == 0:
 				im = convert_bitboard_to_image(board)
-				previous_im = convert_bitboard_to_image(previous_board)
 			else:
 				im = flip_image(convert_bitboard_to_image(board))
 				im = flip_color(im)
-				previous_im = flip_image(convert_bitboard_to_image(previous_board))
-				previous_im = flip_color(previous_im)
-			
-			pair.append(previous_im)
-			pair.append(im)
-			all_pairs.append(pair)
-			
-			previous_board = copy.deepcopy(board)
+				from_coords = flip_coord2d(from_coords)
+				to_coords = flip_coord2d(to_coords)
+
+			index_piece = np.where(im[from_coords] != 0)
+			# index_piece denotes the index in PIECE_TO_INDEX
+			index_piece = index_piece[0][0] # ranges from 0 to 5
+
+			from_coords = flatten_coord2d(from_coords)
+			to_coords = flatten_coord2d(to_coords)
+
+			board.push_san(move)
+
+			# Filling the move_out array
+			move_out_pair.append(im)
+			move_out_pair.append(from_coords)
+			move_out_pair = np.array(move_out_pair)
+
+			move_out.append(move_out_pair)
+
+			# Filling the move_in array
+			move_in_pair.append(im)
+			move_in_pair.append(to_coords)
+			move_in_pair = np.array(move_in_pair)
+
+			move_in_p = "move_in_p%d" % (index_piece + 1)
+			move_in_p = eval(move_in_p)
+			move_in_p.append(move_in_pair)
+		
+move_out = np.array(move_out)
+move_in_p1 = np.array(move_in_p1)
+move_in_p2 = np.array(move_in_p2)
+move_in_p3 = np.array(move_in_p3)
+move_in_p4 = np.array(move_in_p4)
+move_in_p5 = np.array(move_in_p5)
+move_in_p6 = np.array(move_in_p6)
 
 print "Processed %d games out of %d" % (NUM_GAMES, NUM_GAMES)
 print "Saving data..."
 
-output = open('data_%d.pkl' % NUM_GAMES, 'wb')
-pickle.dump(all_pairs, output)
+output = open('move_out_%d.pkl' % NUM_GAMES, 'wb')
+pickle.dump(move_out, output)
+output.close()
+
+output = open('move_in_p1_%d.pkl' % NUM_GAMES, 'wb')
+pickle.dump(move_in_p1, output)
+output.close()
+
+output = open('move_in_p2_%d.pkl' % NUM_GAMES, 'wb')
+pickle.dump(move_in_p2, output)
+output.close()
+
+output = open('move_in_p3_%d.pkl' % NUM_GAMES, 'wb')
+pickle.dump(move_in_p3, output)
+output.close()
+
+output = open('move_in_p4_%d.pkl' % NUM_GAMES, 'wb')
+pickle.dump(move_in_p4, output)
+output.close()
+
+output = open('move_in_p5_%d.pkl' % NUM_GAMES, 'wb')
+pickle.dump(move_in_p5, output)
+output.close()
+
+output = open('move_in_p6_%d.pkl' % NUM_GAMES, 'wb')
+pickle.dump(move_in_p6, output)
 output.close()
 
 print "Done!"
--- src/util.py
+++ src/util.py
@@ -4,6 +4,8 @@ import numpy as np
 
 BOARD_SIZE = (8, 8, 6)
 PIECE_TO_INDEX = {'P' : 0, 'R' : 1, 'N' : 2, 'B' : 3, 'Q' : 4, 'K' : 5}
+Y_TO_CHESSY = {0 : 'a', 1 : 'b', 2 : 'c', 3 : 'd', 4 : 'e', 5 : 'f', 6 : 'g', 7 : 'h'}
+CHESSY_TO_Y = {'a' : 0, 'b' : 1, 'c' : 2, 'd' : 3, 'e' : 4, 'f' : 5, 'g' : 6, 'h' : 7}
 
 # Reads all the chessboard games in the provided
 # pgn file. Returns games
@@ -57,4 +59,34 @@ def flip_color(im):
 	im[indices_white] = -1
 	im[indices_black] = 1
 	return im
+
+def flip_coord2d(coord2d):
+	return (8 - coord2d[0] - 1, coord2d[1])
+
+def coord2d_to_chess_coord(coord2d):
+	chess_coord = Y_TO_CHESSY[coord2d[1]] + str(8 - coord2d[0])
+	return chess_coord
+
+def chess_coord_to_coord2d(chess_coord):
+	return (8 - int(chess_coord[1]), CHESSY_TO_Y[chess_coord[0]])
+
+def flatten_coord2d(coord2d):
+	return ((8 * coord2d[0]) + coord2d[1])
+
+def clip_pieces(prob_dist, im):
+	no_white_indices = np.where(im != 1)
+	no_white_indices = no_white_indices[:2]
+	prob_dist[no_white_indices] = 0
+	return prob_dist
+
+# def clip_move(prob_dist, im, coord):
+# 	piece_coord = coord2d_to_chess_coord(coord)
+
+# 	chess.Move.from_uci("c1c2") in board.legal_moves
+
+
+
+
+
+
 	
