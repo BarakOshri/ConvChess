809f3affb75d0fa4772b649ac3a3dba3f544d9cf
Nishith Khandwala <nishith@stanford.edu>
2015-03-08 19:26:24 -0700

    Implemented functions with tanh

--- src/FastNet/layer_utils.py
+++ src/FastNet/layer_utils.py
@@ -18,6 +18,23 @@ def conv_relu_forward(x, w, b, conv_param):
   cache = (conv_cache, relu_cache)
   return out, cache
 
+def conv_tanh_forward(x, w, b, conv_param):
+  """
+  A convenience layer that performs a convolution followed by a tanh.
+
+  Inputs:
+  - x: Input to the convolutional layer
+  - w, b, conv_param: Weights and parameters for the convolutional layer
+  
+  Returns a tuple of:
+  - out: Output from the tanh
+  - cache: Object to give to the backward pass
+  """
+  a, conv_cache = conv_forward_fast(x, w, b, conv_param)
+  out, tanh_cache = tanh_forward(a)
+  cache = (conv_cache, tanh_cache)
+  return out, cache
+
 
 def conv_relu_backward(dout, cache):
   """
@@ -25,6 +42,15 @@ def conv_relu_backward(dout, cache):
   """
   conv_cache, relu_cache = cache
   da = relu_backward(dout, relu_cache)
+  dx, dw, db = conv_backward_fast(da, conv_cache)
+  return dx, dw, db
+
+def conv_tanh_backward(dout, cache):
+  """
+  Backward pass for the conv-tanh convenience layer.
+  """
+  conv_cache, tanh_cache = cache
+  da = tanh_backward(dout, tanh_cache)
   dx, dw, db = conv_backward_fast(da, conv_cache)
   return dx, dw, db
 
@@ -48,6 +74,24 @@ def conv_relu_pool_forward(x, w, b, conv_param, pool_param):
   cache = (conv_cache, relu_cache, pool_cache)
   return out, cache
 
+def conv_tanh_pool_forward(x, w, b, conv_param, pool_param):
+  """
+  Convenience layer that performs a convolution, a tanh, and a pool.
+
+  Inputs:
+  - x: Input to the convolutional layer
+  - w, b, conv_param: Weights and parameters for the convolutional layer
+  - pool_param: Parameters for the pooling layer
+
+  Returns a tuple of:
+  - out: Output from the pooling layer
+  - cache: Object to give to the backward pass
+  """
+  a, conv_cache = conv_forward_fast(x, w, b, conv_param)
+  s, tanh_cache = tanh_forward(a)
+  out, pool_cache = max_pool_forward_fast(s, pool_param)
+  cache = (conv_cache, tanh_cache, pool_cache)
+  return out, cache
 
 def conv_relu_pool_backward(dout, cache):
   """
@@ -56,6 +100,16 @@ def conv_relu_pool_backward(dout, cache):
   conv_cache, relu_cache, pool_cache = cache
   ds = max_pool_backward_fast(dout, pool_cache)
   da = relu_backward(ds, relu_cache)
+  dx, dw, db = conv_backward_fast(da, conv_cache)
+  return dx, dw, db
+
+def conv_tanh_pool_backward(dout, cache):
+  """
+  Backward pass for the conv-tanh-pool convenience layer
+  """
+  conv_cache, tanh_cache, pool_cache = cache
+  ds = max_pool_backward_fast(dout, pool_cache)
+  da = tanh_backward(ds, tanh_cache)
   dx, dw, db = conv_backward_fast(da, conv_cache)
   return dx, dw, db
 
@@ -77,6 +131,22 @@ def affine_relu_forward(x, w, b):
   cache = (fc_cache, relu_cache)
   return out, cache
 
+def affine_tanh_forward(x, w, b):
+  """
+  Convenience layer that perorms an affine transform followed by a tanh
+
+  Inputs:
+  - x: Input to the affine layer
+  - w, b: Weights for the affine layer
+
+  Returns a tuple of:
+  - out: Output from the tanh
+  - cache: Object to give to the backward pass
+  """
+  a, fc_cache = affine_forward(x, w, b)
+  out, tanh_cache = tanh_forward(a)
+  cache = (fc_cache, tanh_cache)
+  return out, cache
 
 def affine_relu_backward(dout, cache):
   """
@@ -84,5 +154,14 @@ def affine_relu_backward(dout, cache):
   """
   fc_cache, relu_cache = cache
   da = relu_backward(dout, relu_cache)
+  dx, dw, db = affine_backward(da, fc_cache)
+  return dx, dw, db
+
+def affine_tanh_backward(dout, cache):
+  """
+  Backward pass for the affine-tanh convenience layer
+  """
+  fc_cache, tanh_cache = cache
+  da = tanh_backward(dout, tanh_cache)
   dx, dw, db = affine_backward(da, fc_cache)
   return dx, dw, db
